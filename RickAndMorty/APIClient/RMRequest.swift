b0185a5f6e5754e8bea93787cf318a4792813721
Joao Gripp <corguinha@gmail.com>
2024-02-15 00:48:35 +0000

    Part 2 finished - Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Joao Gripp on 15/02/24.
+//
+
+import Foundation
+
+/// Object that represents a single API Call
+final class RMRequest {
+    
+}
